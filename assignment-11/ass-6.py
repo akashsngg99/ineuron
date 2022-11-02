09758c547a09b73aa638a66ce228a3783b71b846
akashsngg99 <49447912+akashsngg99@users.noreply.github.com>
2022-11-02 22:48:27 +0530

    Add files via upload

---
+++ assignment-11/ass-6.py
@@ -0,0 +1,9 @@
+n=int(input("Enter number : "))
+count =0
+for i in range(1,200):
+    if i%2==0:
+        print(i)
+        count+=1
+    if count==n:
+        break
+
---
+++ assignment-11/ass-7.py
@@ -0,0 +1,9 @@
+n=int(input("Enter number : "))
+count =0
+for i in reversed(range(1,20)):
+    if i%2==0:
+        print(i)
+        count+=1
+    if count==n:
+        break
+
