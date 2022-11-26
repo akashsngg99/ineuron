eb2e9334797f588056d0b582d2a8363c77d8e6c8
akashsngg99 <49447912+akashsngg99@users.noreply.github.com>
2022-11-26 10:48:22 +0530

    Add files via upload

---
+++ assignment-22/ass-1.py
@@ -0,0 +1,7 @@
+def recur_sum(n):
+   if n <= 1:
+       return n
+   else:
+       return n + recur_sum(n-1)
+
+recur_sum(10)
---
+++ assignment-22/ass-8.py
@@ -0,0 +1,7 @@
+def repN(N,K):
+    if(N+1==K): return 
+    print(K**3,end=" ")
+    K=K+1
+    repN(N,K)
+
+repN(10,1)
